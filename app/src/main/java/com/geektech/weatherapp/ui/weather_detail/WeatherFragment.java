917284729da511da063d300a53799da6105c8b6e
Uliq <ulukbek1999@gmail.com>
2021-11-22 18:34:48 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/weatherapp/ui/weather_detail/WeatherFragment.java
@@ -0,0 +1,32 @@
+package com.geektech.weatherapp.ui.weather_detail;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.geektech.weatherapp.R;
+
+public class WeatherFragment extends Fragment {
+
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+
+    }
+}
